2e9feea7634f9b41c0026b54bfac4f30cea7a49e
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-01-03 21:44:40 -0700

    getting rid of tournaments for now

--- app/models/tournament_creator.rb
+++ app/models/tournament_creator.rb
@@ -1,42 +1,42 @@
-class TournamentCreator
-  include ActiveModel::Model
-
-  attr_accessor :players, :name, :tournament, :end_date
-
-  validate :no_duplicate_players, :must_have_two_players, :has_future_date
-
-  def initialize(params)
-    @name = params[:name]
-    @players = params[:players]
-    @end_date = params[:end_date].to_date
-  end
-
-  def save
-    return false unless valid?
-    @tournament = Tournament.create(name: name, end_date: end_date)
-    @tournament.players << Player.find(players)
-    create_matchups
-    true
-  end
-
-  private
-
-  def create_matchups
-    players.combination(2).each do |combo|
-      tournament.matchups << Matchup.create(primary_id: combo.first,
-                                            secondary_id: combo.second)
-    end
-  end
-
-  def must_have_two_players
-    errors.add :base, 'Must have at least two players' if players.count < 2
-  end
-
-  def has_future_date
-    errors.add :base, 'End date must be in the future' if end_date < Date.current
-  end
-
-  def no_duplicate_players
-    errors.add :base, 'Cannot have duplicate players' if players.count != players.uniq.count
-  end
-end
+# class TournamentCreator
+#   include ActiveModel::Model
+#
+#   attr_accessor :players, :name, :tournament, :end_date
+#
+#   validate :no_duplicate_players, :must_have_two_players, :has_future_date
+#
+#   def initialize(params)
+#     @name = params[:name]
+#     @players = params[:players]
+#     @end_date = params[:end_date].to_date
+#   end
+#
+#   def save
+#     return false unless valid?
+#     @tournament = Tournament.create(name: name, end_date: end_date)
+#     @tournament.players << Player.find(players)
+#     create_matchups
+#     true
+#   end
+#
+#   private
+#
+#   def create_matchups
+#     players.combination(2).each do |combo|
+#       tournament.matchups << Matchup.create(primary_id: combo.first,
+#                                             secondary_id: combo.second)
+#     end
+#   end
+#
+#   def must_have_two_players
+#     errors.add :base, 'Must have at least two players' if players.count < 2
+#   end
+#
+#   def has_future_date
+#     errors.add :base, 'End date must be in the future' if end_date < Date.current
+#   end
+#
+#   def no_duplicate_players
+#     errors.add :base, 'Cannot have duplicate players' if players.count != players.uniq.count
+#   end
+# end
--- spec/models/tournament_creator_spec.rb
+++ spec/models/tournament_creator_spec.rb
@@ -1,30 +1,30 @@
-require 'rails_helper'
-
-describe TournamentCreator do
-  describe '#save' do
-    let!(:player_1) { Player.create name: 'Player 1' }
-    let!(:player_2) { Player.create name: 'Player 2' }
-    let(:players_ids) { [player_1.id, player_2.id] }
-    let(:tournament_name) { 'Some Name' }
-    let(:end_date) { 1.week.from_now }
-    let(:params) { { name: tournament_name,
-                     players: players_ids,
-                     end_date: end_date }}
-
-    context 'valid tournament' do
-      subject { TournamentCreator.new(params)}
-
-      before do
-        subject.save
-      end
-
-      it 'returns the tournament with the correct number of entries' do
-        expect(subject.tournament.players.count).to be 2
-      end
-
-      it 'sets the tournaments name' do
-        expect(subject.tournament.name).to eq tournament_name
-      end
-    end
-  end
-end
+# require 'rails_helper'
+#
+# describe TournamentCreator do
+#   describe '#save' do
+#     let!(:player_1) { Player.create name: 'Player 1' }
+#     let!(:player_2) { Player.create name: 'Player 2' }
+#     let(:players_ids) { [player_1.id, player_2.id] }
+#     let(:tournament_name) { 'Some Name' }
+#     let(:end_date) { 1.week.from_now }
+#     let(:params) { { name: tournament_name,
+#                      players: players_ids,
+#                      end_date: end_date }}
+#
+#     context 'valid tournament' do
+#       subject { TournamentCreator.new(params)}
+#
+#       before do
+#         subject.save
+#       end
+#
+#       it 'returns the tournament with the correct number of entries' do
+#         expect(subject.tournament.players.count).to be 2
+#       end
+#
+#       it 'sets the tournaments name' do
+#         expect(subject.tournament.name).to eq tournament_name
+#       end
+#     end
+#   end
+# end
